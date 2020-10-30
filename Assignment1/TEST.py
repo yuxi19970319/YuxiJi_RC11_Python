d0d3fe5f875faa56bf8bc0f6092c5f0007d4185a
yuxi19970319 <72449551+yuxi19970319@users.noreply.github.com>
2020-10-31 03:33:04 +0800

    delete

--- Assignment1/TEST.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("D:\\assignment\python\\file\YuxiJi_RC11_Python\Assignment1\csvFiles\\artwork_data.csv", encoding = 'utf-8-sig') as dataFile:
-    artReader = csv.DictReader(dataFile)
-    line1=next(artReader)
-    print(line1)
